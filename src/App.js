a4890b16a06047fae96a8dbeac56b8f9d366ac06
kingjaejun <saqxtmxhf888@naver.com>
2019-10-02 18:38:03 +0900

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import  styled,{ createGlobalStyle, ThemeProvider } from 'styled-components';
+import themes from './themes'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const GlobalStyle = createGlobalStyle`
+  html {
+    padding:0;
+    margin:0;
+  }
+`;
+const Container =styled.div`
+  height:100vh;
+  width:100%;
+  background-color:pink;
+`;
+const Card = styled.div`
+  background-color:white;
+
+`;
+const Button = styled.button`
+  border-radius:30px;
+  padding:25px 15px;
+  background-color: ${props => props.theme.successColor}
+`;
+class App extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={themes}>
+      <Container>
+        <Form />
+      </Container>
+     </ThemeProvider>
+    );
+  }   
 }
+const Form =()=>(
+  <Card>
+    <Button>hello</Button>
+  </Card>
+)
+
 
 export default App;
